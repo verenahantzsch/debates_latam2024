500c0bd5429dcc7ff59c0690ca212cf2b25da1c2
CVFH <caro.franco.brc@gmail.com>
2024-03-14 19:41:57 -0300

    cree base agregada, faltan detalles. no guarde aun. revise encuestas posibles de descarga

---
+++ tesis_doctorado/datav2023/APUNTES_TEMPORAL_ENCUESTASFALTANTES.R
@@ -0,0 +1,88 @@
+# PENDIENTE VER QUE CONSIDERA RELIABLE
+# EVENTUALLY: MIRAR "VOTO" PROPIAMENTE DICHO 
+
+# ELECCIONES POTENCIALES PENDIENTES DE DESCARGA
+# 
+# anteriores no, solo texto
+# arg 1999 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_presidenciales_de_Argentina_de_1999#Encuestas_de_opini%C3%B3n
+# arg 2003 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_presidenciales_de_Argentina_de_2003#Encuestas_de_opini%C3%B3n 
+# 
+# bolivia 2002 no, anteriores tampoco
+# bolivia 2005 solo texto
+# bolivia 2009 solo texto 
+# bolivia 2015 solo texto
+# 
+# brasil 1998 no
+# brasil 2022 Anexo 
+# https://es.wikipedia.org/wiki/Anexo:Sondeos_de_opini%C3%B3n_para_las_elecciones_generales_de_Brasil_de_2022#2022
+# https://es.wikipedia.org/wiki/Anexo:Sondeos_de_opini%C3%B3n_para_las_elecciones_generales_de_Brasil_de_2022#2022_2
+# 
+# chile 1989 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecci%C3%B3n_presidencial_de_Chile_de_1989#Encuestas 
+# chile 1993 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecci%C3%B3n_presidencial_de_Chile_de_1993#Encuestas 
+# (son tan poquitas que quizas conviene pegar manual)
+# chile 1999-2000 PAGINA GENERAL solo primera vuelta https://es.wikipedia.org/wiki/Elecci%C3%B3n_presidencial_de_Chile_de_1999-2000#Encuestas 
+# (son tan poquitas que quizas conviene pegar manual)
+# chile 2005-2006 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecci%C3%B3n_presidencial_de_Chile_de_2005-2006 
+# chile 2013 faltaba segunda vuelta ANEXO https://es.wikipedia.org/wiki/Anexo:Encuestas_de_la_elecci%C3%B3n_presidencial_de_Chile_de_2013#Segunda_vuelta 
+# 
+# anteriores no 
+# colombia 2002 no
+# colombia 2006 no
+# colombia 2022 
+# ANEXO primera vuelta https://es.wikipedia.org/wiki/Anexo:Sondeos_de_intenci%C3%B3n_de_voto_para_las_elecciones_presidenciales_de_Colombia_de_2022#Candidaturas_oficiales
+# ANEXO segunda vuelta https://es.wikipedia.org/wiki/Anexo:Sondeos_de_intenci%C3%B3n_de_voto_para_las_elecciones_presidenciales_de_Colombia_de_2022#Segunda_vuelta 
+# colombia 2014 faltaba segunda vuelta PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_presidenciales_de_Colombia_de_2014#Segunda_vuelta
+# 
+# anteriores no
+# costa rica 2002 no 
+# costa rica 2006 no 
+# costa rica 2010 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Costa_Rica_de_2010#Encuestas 
+# costa rica 2022 
+# ANEXO primera vuelta  https://es.wikipedia.org/wiki/Anexo:Sondeos_de_las_elecciones_presidenciales_de_Costa_Rica_de_2022#Primera_Ronda 
+# ANEXO segunda vuelta https://es.wikipedia.org/wiki/Anexo:Sondeos_de_las_elecciones_presidenciales_de_Costa_Rica_de_2022#Segunda_ronda
+# 
+# anteriores no 
+# ecuador 2009 no 
+# ecuador 2023 no
+# ecuador 2017 segunda vuelta no 
+# 
+# anteriores no 
+# el salvador 2014 no
+# el salvador 2024 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecci%C3%B3n_presidencial_de_El_Salvador_de_2024#Encuestas_de_opini%C3%B3n 
+# 
+# anteriores no
+# guatemala 1990 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Guatemala_de_1990#Encuestas 
+# guatemala 1999 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Guatemala_de_1999#Encuestas 
+# guatemala 2003 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Guatemala_de_2003#Encuestas 
+# guatemala 2007 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Guatemala_de_2007#Encuestas 
+# guatemala 2011 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Guatemala_de_2011#Encuestas 
+# guatemala 2015 segunda vuelta no
+# 
+# anteriores no
+# mexico 1994 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_federales_de_M%C3%A9xico_de_1994#Encuestas[11]%E2%80%8B 
+# mexico 2000 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_federales_de_M%C3%A9xico_de_2000#Encuestas 
+# 
+# anteriores no
+# panama 2014 no 
+# 
+# anteriores no
+# paraguay 2008 no 
+# paraguay 2018 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Paraguay_de_2018#Encuestas 
+# paraguay 2023 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Paraguay_de_2023#Encuestas_de_opini%C3%B3n 
+# 
+# anteriores no 
+# peru 1990 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Per%C3%BA_de_1990#Encuestas 
+# peru 1995 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Per%C3%BA_de_1995#Encuestas 
+# peru 2000
+# ANEXO primera vuelta
+# https://es.wikipedia.org/wiki/Anexo:Sondeos_de_intenci%C3%B3n_de_voto_para_las_elecciones_presidenciales_de_Per%C3%BA_de_2000#Primera_vuelta
+# ANEXO segunda vuelta
+# https://es.wikipedia.org/wiki/Anexo:Sondeos_de_intenci%C3%B3n_de_voto_para_las_elecciones_presidenciales_de_Per%C3%BA_de_2000#Intenci%C3%B3n_de_voto_2
+# peru 2000 y 2001 PAGINA GENERAL https://es.wikipedia.org/wiki/Elecciones_generales_de_Per%C3%BA_de_2001#Encuestas 
+# 
+# anteriores no
+# uruguay 2004 no 
+# 
+# anteriores no
+# venezuela 2018 PAGINA PRINCIPAL https://es.wikipedia.org/wiki/Elecciones_presidenciales_de_Venezuela_de_2018#Encuestas_de_opini%C3%B3n 
+# venezuela 2000 no 
--- tesis_doctorado/datav2023/creacion_base_elecciones_cantu.R
+++ tesis_doctorado/datav2023/creacion_base_elecciones_cantu.R
@@ -1,7 +1,7 @@
 # Este script crea base de datos cuya U de A son las elecciones, a partir de datos prestados por cantu carreras
 # marzo 2024
 
-
+library(tidyverse)
 
 # PRIMERA PARTE: EXTRACCION DE CANDIDATOS DE BASE CANTU CARRERAS #################
 
@@ -81,37 +81,50 @@ data <- data[data$daysbeforeED<=120,]
 
 rm(Lat_data, Lat_update)
 
+# algunas limpiezas necesarias
+
+data <- data %>% 
+  subset(electionid!="Argentina 2011-08-14") %>%  # eliminamos elecciones primarias argentina
+  mutate(round = ifelse(country=="Argentina"&round(electionyr)==2011,1,round)) %>% 
+  mutate(turnout = ifelse(country=="Brazil"&round(electionyr)==2014&round==1, 80.61, turnout),
+         turnout = ifelse(country=="Brazil"&round(electionyr)==2014&round==2, 78.90, turnout)) %>% # habia problemas con decimales en el reporte de este numero para distintos items de esta eleccion
+  subset(!(country=="Brazil"&round(electionyr)==2010&turnout==81.90)) %>%  # eliminamos base mas incompleta dupilcada de brasil para esta eleccion  
+  mutate(id_polldatapoint = row_number()) 
+
+reserva <- data 
 # cargo data con nombres de candidatos
 # esta base fue creada en limpieza_unique_candidates y luego completada manualmente
 
 setwd("/home/carolina/Documents/Proyectos R/debates_latam2024/tesis_doctorado/datav2023")
-missing_names <-  readxl::read_xlsx("unique_candidates_tomerge_incompletos_completado.xlsx")
+missing_names <- readxl::read_xlsx("unique_candidates_tomerge_incompletos_completado.xlsx")
 
 # Uno
-data <- data %>% 
-  dplyr::rename("nombres_candidatos" = c_names) %>% # hago este paso ahora para no pisar data justamente
-  left_join(missing_names)
 
-data <- data %>% 
+
+data_con_nombres <- data %>% 
+  dplyr::rename("nombres_candidatos" = c_names) %>% # hago este paso ahora para no pisar data justamente
+  left_join(missing_names) 
+
+data_con_nombres <- data_con_nombres %>% 
   mutate(nombres_candidatos = ifelse(is.na(nombres_candidatos),
                                      c_names,
-                                     nombres_candidatos))  %>% 
-  mutate(electionyr = ifelse(country=="Chile"&round(electionyr)==2009, 2010, electionyr)) %>% # para hacer luego compatible con base carolina
-  subset(electionid!="Argentina 2011-08-14") %>%  # eliminamos elecciones primarias argentina
-  mutate(round = ifelse(country=="Argentina"&round(electionyr)==2011,1,round)) %>% 
-  subset(!(country=="Brazil"&round(electionyr)==2010&turnout==81.90)) %>%  # eliminamos base mas incompleta dupilcada de brasil para esta eleccion  
-  mutate(id_polldatapoint = row_number()) 
+                                     nombres_candidatos)) %>% 
+  mutate(nombres_candidatos = str_trim(nombres_candidatos, "both"),
+         nombres_candidatos = iconv(nombres_candidatos, to = "ASCII//TRANSLIT"))  %>% 
+  mutate(country = ifelse(country=="Brazil", "Brasil", country))  %>% # para hacer luego compatible con base carolina
+  mutate(electionyr = ifelse(country=="Chile"&round(electionyr)==2009, 2010, electionyr))  # para hacer luego compatible con base carolina
+  
 
 rm(missing_names)
 
 # chequeo. deberiamos tener al menos 32 elecciones (con debates) + otras sin debates
 
-u_elect <- data$electionid %>% unique() # por ahora tenemos 64 elecciones, generales y ballotage
+u_elect <- data$electionid %>% unique() # por ahora tenemos 63 elecciones, generales y ballotage
 
 
 # PRIMERA MITAD DE LA BASE: LADO CANTU ###########
 
-data_candidatos <- data %>% 
+data_candidatos <- data_con_nombres %>% 
   # primero agrupamos por todas las variables que correspondan al nivel "candidatos", perdiendo el nivel de "polls"
   # tengo ademas que seleccionar todas las variables que quiero retener del nivel electoral 
   group_by(electionid, electionyr, country, round, enpp, turnout, espv, regime, nombres_candidatos, gov_, inc_) %>% 
@@ -120,6 +133,7 @@ data_candidatos <- data %>%
          vote_ = mean(vote_)) %>% # esta es porque habia unas inconsistencias menores en data de brasil (0.01 diferencia entre filas)
   ungroup() 
   
+u_elect <- data_candidatos$electionid %>% unique() # por ahora tenemos 63 elecciones, generales y ballotage
 
 # ahora agrupamos por todas las variables que corresponden al nivel "elecciones"
 # tenemos que usar algunos indicadores del nivel "candidatos" para construir indicadores de nivel superior, de "elecciones"
@@ -133,69 +147,43 @@ data_competencia <- data_candidatos %>%
 data_candidatos <- data_candidatos %>% 
   left_join(data_competencia) %>% 
   mutate(frontrunner = ifelse(rank_polls==1,1,0),
+         challenger = ifelse(rank_polls==2,1,0),
          winner = ifelse(rank_elections==1,1,0),
          incumbent = ifelse(gov_==1,1,0))
 
 data_incumbents <- data_candidatos %>% 
   subset(incumbent==1) %>% 
-  select(electionid, country, round, nombres_candidatos) %>% 
-  dplyr::rename("incumbente" = nombres_candidatos)
+  select(electionid, electionyr, country, round, nombres_candidatos) %>% 
+  dplyr::rename("incumbente" = nombres_candidatos) %>% 
+  unique()
 
 data_frontrunner <- data_candidatos %>% 
   subset(frontrunner==1) %>% 
-  select(electionid, country, round, nombres_candidatos) %>% 
+  select(electionid, electionyr, country, round, nombres_candidatos) %>% 
   dplyr::rename("frontrunner" = nombres_candidatos)
 
 data_winner <- data_candidatos %>% 
   subset(winner==1) %>% 
-  select(electionid, country, round, nombres_candidatos) %>% 
+  select(electionid, electionyr, country, round, nombres_candidatos) %>% 
   dplyr::rename("winner" = nombres_candidatos)
 
-
+data_challenger <- data_candidatos %>% 
+  subset(challenger==1) %>% 
+  select(electionid, electionyr, country, round, nombres_candidatos) %>% 
+  dplyr::rename("challenger" = nombres_candidatos)
+  
 data_elecciones <- data_candidatos %>% 
   group_by(electionid, electionyr, country, round, enpp, turnout, espv, regime) %>% 
   summarise(sd_mean_encuestas_candidato = sd(mean_encuestas_candidato),
             sd_vote_ = sd(vote_)) %>% 
-  left_join(data_incumbents) %>% 
+ left_join(data_incumbents) %>% 
   left_join(data_frontrunner) %>% 
   left_join(data_winner) %>% 
+  left_join(data_challenger) %>% 
   ungroup()
 
-# PENDIENTES 13 MARZO 2024 ####
-# todavía hay algunas inconsistencias a resolver luego:
-# data brasil 2014 o alguna otra parece estar otra vez replicada.
-# varios missing en incumbencia. tiene sentido, dado que tenemos 399 incumbentes y 64 elecciones. Esta variable me deja dudando
-# aunque conociendo inestabilidad latam, tb puede ser
-# ademas pendiente pensar indicadores estado de competencia. Revisar variable enpp  Effective number of political parties at election
-# abajo dejo apuntes Celeste
 
-# sugerencias de indices de celeste Ratto
-
-# En cuanto a las dimensiones del voto o del sistema de partidos: 
-#   • Indices de fragmentación electoral y parlamentaria de Rae. 
-# 
-# • Indices del número efectivo de partidos, electorales y parlamentarios: 
-#   ◦ Indices (electoral y parlamentario) sugeridos por Laakso y Taagepera y Taagepera y Shugart. 
-# ◦ Indices de hiperfraccionamiento (electoral y parlamentario) de Kesselman y Wildgen. 
-# ◦ Indices (electoral y parlamentario) de Molinar. 
-# 
-# • Indices de concentración electoral y parlamentaria. 
-# 
-# • Indices de competitividad electoral y parlamentaria. 
-# 
-# • Indice de polarización de Sartori. 
-# • Indices de polarización ponderada (electoral y parlamentaria). 
-# 
-# • Indices de volatilidad (electoral y parlamentaria) total, entre bloques e intrabloques, propuestos por Pedersen y Bartolini y Mair (caben distintas aplicaciones, según la dimensión que resulte relevante: izquierda-derecha, centro- periferia, etc.). 
-# 
-# • Indice de voto dual, de Arian y Weiss. 
-# 
-# • Indice de voto regionalista. 
-# • Indice de voto regionalista diferenciado. 
-# • Indice de voto regional diferenciado, de Lee. 
-
-
-# SEGUNDA MITAD DE LA BASE: LADO CAROLINA
+# SEGUNDA MITAD DE LA BASE: LADO CAROLINA #######################
 # Nos interesa indicador de: elecciones con debate Y elecciones con debate con winner presente
 # eventualmente, elecciones con dos ppales, (facil, crear variable challenger como arriba)
 # eventualmente, elecciones con debates org por medios. 
@@ -257,11 +245,14 @@ data_elecciones_carolina <-  readxl::read_xlsx("base_eleccionesv2023.xlsx") # ba
 #   subset((country=="Guatemala"&round(electionyr)==2015)) # efectivamente falta segunda vuelta, Domingo 25 de octubre de 2015 (2.ª vuelta) https://es.wikipedia.org/wiki/Elecciones_generales_de_Guatemala_de_2015
 # data_missing_Ecuador <- data  %>%   
 #   subset((country=="Ecuador"&round(electionyr)==2017)) # efectivamente falta segunda vuelta, Domingo 2 de abril de 2017 (Segunda vuelta) https://es.wikipedia.org/wiki/Elecciones_presidenciales_de_Ecuador_de_2017
+#data_duplicated_Brasil <- data  %>%   
+#     subset((country=="Brazil"&round(electionyr)==2014))  
+
 
 # AHORA SI HACEMOS JOIN ########
 
 # primero creamos base anual relevante
-# tengo que separar ronda 1 de 2. PENDIENTE 
+# tengo que separar ronda 1 de 2.  
 
 # creamos loop para agregar rondas faltantes cuando corresponda
 for (i in 1:nrow(data_elecciones_carolina)) {
@@ -272,25 +263,153 @@ for (i in 1:nrow(data_elecciones_carolina)) {
   }
 }
 
+# uno data sobre incumbentes etc a base de debates para poder identificar debates con frontrunner presente
+
+# primero tenemos que ajustar estas bases para el join
+
+data_incumbents <- data_incumbents %>% 
+  mutate(ncat_eleccion = round(electionyr)) %>% 
+  dplyr::rename("cat_pais" = country, "ncat_ronda" = round) %>% 
+  select(-electionid, -electionyr)
+
+data_frontrunner <- data_frontrunner %>% 
+  mutate(ncat_eleccion = round(electionyr)) %>% 
+  dplyr::rename("cat_pais" = country, "ncat_ronda" = round) %>% 
+  select(-electionid, -electionyr)
+
+data_winner <- data_winner %>% 
+  mutate(ncat_eleccion = round(electionyr)) %>% 
+  dplyr::rename("cat_pais" = country, "ncat_ronda" = round) %>% 
+  select(-electionid, -electionyr)
+
+data_challenger <- data_challenger %>% 
+  mutate(ncat_eleccion = round(electionyr)) %>% 
+  dplyr::rename("cat_pais" = country, "ncat_ronda" = round) %>% 
+  select(-electionid, -electionyr)
+
+# hacemos el join
+base2 <- base %>% 
+  mutate(str_presentes = str_trim(str_presentes, "both"),
+         str_presentes = iconv(str_presentes, to = "ASCII//TRANSLIT")) %>% 
+  left_join(data_incumbents) %>% 
+  left_join(data_frontrunner) %>% 
+  left_join(data_winner) %>% 
+  left_join(data_challenger) %>% 
+  mutate(frontrunner_presente = ifelse(str_detect(str_presentes, frontrunner),1,0),
+         incumbente_presente = ifelse(str_detect(str_presentes, incumbente),1,0),
+         winner_presente = ifelse(str_detect(str_presentes, winner),1,0),
+         challenger_presente = ifelse(str_detect(str_presentes, challenger),1,0),
+         frontycha_presentes = ifelse(str_detect(str_presentes, frontrunner)&str_detect(str_presentes, challenger),1,0))
+
 # identifico elecciones con debates
-base_elecciones_con_debates <- base %>% 
+base_elecciones_con_debates <- base2 %>% 
   group_by(cat_pais, ncat_eleccion, ncat_ronda) %>% 
-  summarise(n_debates_eleccion = n()) 
+  summarise(n_debates_eleccion = n(),
+            n_debates_frontrunner_presente = sum(frontrunner_presente, na.rm=T),
+            n_debates_frontycha_presentes = sum(frontycha_presentes, na.rm=T)) %>% 
+  mutate(dico_debates_eleccion = ifelse(n_debates_eleccion>=1,1,0),
+         dico_frontrunner_presente = ifelse(n_debates_frontrunner_presente>=1,1,0),
+         dico_frontycha_presentes = ifelse(n_debates_frontycha_presentes>=1,1,0))
 
 data_elecciones_carolina2 <- data_elecciones_carolina %>% 
   dplyr::rename("ncat_ronda" = cat_ballotage) %>% 
   left_join(base_elecciones_con_debates ) %>% 
-  mutate(n_debates_eleccion = ifelse(is.na(n_debates_eleccion), 0, n_debates_eleccion))
+  mutate(n_debates_eleccion = ifelse(is.na(n_debates_eleccion), 0, n_debates_eleccion),
+         n_debates_frontrunner_presente = ifelse(is.na(n_debates_frontrunner_presente), 0, n_debates_frontrunner_presente),
+         dico_debates_eleccion = ifelse(is.na(dico_debates_eleccion), 0, dico_debates_eleccion),
+         dico_frontrunner_presente = ifelse(is.na(dico_frontrunner_presente), 0, dico_frontrunner_presente),
+         dico_frontycha_presentes = ifelse(is.na(dico_frontycha_presentes), 0, dico_frontycha_presentes))
 
 data_agregada <- data_elecciones %>% 
   mutate(electionyr = round(electionyr)) %>% 
   dplyr::rename("ncat_eleccion" = electionyr, "cat_pais" = country, "ncat_ronda" = round) %>% 
-  mutate( cat_pais = ifelse(cat_pais=="Brazil", "Brasil", cat_pais)) %>% 
   left_join(data_elecciones_carolina2)
 
+u_elec <- data_elecciones$electionid %>% unique()
+
+
+# PENDIENTE  !!!!! ######
 # problemas con esta base: otra vez tengo desbalance en 1/ 0 
 # PENDIENTE: CREAR VARIABLE C DICO DEBATE C/ LEAD Y C CHALLENGER. VA A HABER QUE HACER VARIOS JOINS CRUZADOS
 # TB variable de debates en años previos
 # Tb pensar indicadores adecuados para competitividad
+# PENDIENTES 13 MARZO 2024 ####
+# todavía hay algunas inconsistencias a resolver luego:
+# data brasil 2014 o alguna otra parece estar otra vez replicada.
+# varios missing en incumbencia. tiene sentido, dado que tenemos 399 incumbentes y 64 elecciones. Esta variable me deja dudando
+# aunque conociendo inestabilidad latam, tb puede ser
+# ademas pendiente pensar indicadores estado de competencia. Revisar variable enpp  Effective number of political parties at election
+# abajo dejo apuntes Celeste
 
+# sugerencias de indices de celeste Ratto
+
+# En cuanto a las dimensiones del voto o del sistema de partidos: 
+#   • Indices de fragmentación electoral y parlamentaria de Rae. 
+# 
+# • Indices del número efectivo de partidos, electorales y parlamentarios: 
+#   ◦ Indices (electoral y parlamentario) sugeridos por Laakso y Taagepera y Taagepera y Shugart. 
+# ◦ Indices de hiperfraccionamiento (electoral y parlamentario) de Kesselman y Wildgen. 
+# ◦ Indices (electoral y parlamentario) de Molinar. 
+# 
+# • Indices de concentración electoral y parlamentaria. 
+# 
+# • Indices de competitividad electoral y parlamentaria. 
+# 
+# • Indice de polarización de Sartori. 
+# • Indices de polarización ponderada (electoral y parlamentaria). 
+# 
+# • Indices de volatilidad (electoral y parlamentaria) total, entre bloques e intrabloques, propuestos por Pedersen y Bartolini y Mair (caben distintas aplicaciones, según la dimensión que resulte relevante: izquierda-derecha, centro- periferia, etc.). 
+# 
+# • Indice de voto dual, de Arian y Weiss. 
+# 
+# • Indice de voto regionalista. 
+# • Indice de voto regionalista diferenciado. 
+# • Indice de voto regional diferenciado, de Lee. 
+
+
+
+# CHECK DE DUPLICADOS ################## 
+check_agrupamiento <- data_agregada %>%
+  # mutate(electionyr = ifelse(country=="Chile"&round(electionyr)==2009, 2010, electionyr)) %>% # para hacer luego compatible con base carolina
+  # subset(electionid!="Argentina 2011-08-14") %>%  # eliminamos elecciones primarias argentina
+  # mutate(round = ifelse(country=="Argentina"&round(electionyr)==2011,1,round)) %>%
+  # mutate(turnout = ifelse(country=="Brazil"&round(electionyr)==2014&round==1, 80.61, turnout),
+  #        turnout = ifelse(country=="Brazil"&round(electionyr)==2014&round==2, 78.90, turnout)) %>% # habia problemas con decimales en el reporte de este numero para distintos items de esta eleccion
+  # subset(!(country=="Brazil"&round(electionyr)==2010&turnout==81.90)) %>%  # eliminamos base mas incompleta dupilcada de brasil para esta eleccion
+  # mutate(id_polldatapoint = row_number())  %>%
+  #select(electionid, electionyr, country, round, enpp, turnout, espv, regime) %>%
+  select(electionid, ncat_eleccion, cat_pais, ncat_ronda, enpp, turnout, espv, regime) %>%
+  unique()
+u_elect <- check_agrupamiento$electionid %>% unique()
+
+#Find duplicated rows in column A
+duplicated_rows <- data_incumbents[duplicated(data_incumbents$electionid) | duplicated(data_incumbents$electionid, fromLast = TRUE), ]
+
+# Print the duplicated rows
+print(duplicated_rows)
+# Function to identify columns where two rows differ
+identify_differing_columns <- function(df) {
+  # Initialize an empty vector to store column indices
+  differing_columns <- c()
   
+  # Loop through each column of the dataframe
+  for (col in 1:ncol(df)) {
+    # Check if the values in the column are different between the two rows
+    if (!identical(df[1, col], df[2, col])) {
+      differing_columns <- c(differing_columns, col)
+    }
+  }
+  
+  return(differing_columns)
+}
+
+# Identify columns where two rows differ
+differing_columns <- identify_differing_columns(duplicated_rows)
+
+# Print the column indices
+print("Columns where two rows differ:")
+print(differing_columns)
+
+# Print the column names
+print("Column names where two rows differ:")
+print(colnames(duplicated_rows)[differing_columns])
